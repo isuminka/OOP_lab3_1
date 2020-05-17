fe4f24e45563d880cb2a4cd1aba17100ff39bdcb
Anna <anna.antip34@gmail.com>
2020-05-17 06:17:55 +0300

    Add project files.

---
+++ OOP_lab3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP_lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string[] words = str.Split(new char[] { '\n', '\r', ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*', '/', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            str = "";
+
+            for (int i = 0; i < words.Length; i += 2)
+            {
+                str += words[i] + " ";
+            }
+            
+            words = str.Split(new char[] { '\n', '\r', ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*', '/', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            str = "";
+
+            for (int i = 0; i < words.Length; ++i)
+            {
+                if (words[i][0] != words[i][words[i].Length - 1])
+                {
+                    str += words[i] + " ";
+                }
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
